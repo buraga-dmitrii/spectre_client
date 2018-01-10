4677660b4bdfe4e17827b7f8fb02bbb2e755cc6c
Buraga Dmitrii <buraga.dmitrii@gmail.com>
2018-01-10 21:03:10 +0200

    Removed actions from Transaction controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,80 +1,28 @@
 class TransactionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_transaction, only: [:show, :edit, :update, :destroy]
 
-  # GET /transactions
-  # GET /transactions.json
   def index
-    if params[:account_id]
-      @account = Account.where(id: params[:account_id]).first
-      @transactions = @account.transactions     
-    else
-     redirect_to logins_path
-    end 
-  end
+    unless transaction_params[:account_id]
+      redirect_to logins_path and return
+    end
 
-  # GET /transactions/1
-  # GET /transactions/1.json
-  def show
-  end
+    @account = Account.joins(login: :customer)
+      .where(id: transaction_params[:account_id])
+      .where('customers.id = ?', current_user.customer.id)
+      .first
 
-  # GET /transactions/new
-  def new
-    @transaction = Transaction.new
-  end
-
-  # GET /transactions/1/edit
-  def edit
-  end
-
-  # POST /transactions
-  # POST /transactions.json
-  def create
-    @transaction = Transaction.new(transaction_params)
-
-    respond_to do |format|
-      if @transaction.save
-        format.html { redirect_to @transaction, notice: 'Transaction was successfully created.' }
-        format.json { render :show, status: :created, location: @transaction }
-      else
-        format.html { render :new }
-        format.json { render json: @transaction.errors, status: :unprocessable_entity }
-      end
+    if @account
+      @transactions = @account.transactions
+    else  
+      redirect_to logins_path 
     end
   end
 
-  # PATCH/PUT /transactions/1
-  # PATCH/PUT /transactions/1.json
-  def update
-    respond_to do |format|
-      if @transaction.update(transaction_params)
-        format.html { redirect_to @transaction, notice: 'Transaction was successfully updated.' }
-        format.json { render :show, status: :ok, location: @transaction }
-      else
-        format.html { render :edit }
-        format.json { render json: @transaction.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /transactions/1
-  # DELETE /transactions/1.json
-  def destroy
-    @transaction.destroy
-    respond_to do |format|
-      format.html { redirect_to transactions_url, notice: 'Transaction was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
+  
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_transaction
-      @transaction = Transaction.find(params[:id])
+
+    def transaction_params
+      params.permit(:account_id)
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def transaction_params
-      params.require(:transaction).permit(:transaction_id, :category, :currency, :amount, :description, :made_on, :mode, :status, :account_id)
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  resources :transactions
+  resources :transactions, only: [:index]
   resources :accounts
   resources :logins
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root to: "logins#index"
   get :login_create, to: 'logins#login_create'
