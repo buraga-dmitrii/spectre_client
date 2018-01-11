a9d1e5a4ad66a10144b2389e922022d53c32f6c4
Buraga Dmitrii <buraga.dmitrii@gmail.com>
2018-01-11 11:01:55 +0200

    Removed actions from LoginsController

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,9 +1,7 @@
 class LoginsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_login, only: [:show, :edit, :update, :destroy, :login_refresh, :login_reconnect]
+  before_action :set_login, only: [:destroy, :login_refresh, :login_reconnect]
 
-  # GET /logins
-  # GET /logins.json
   def index
     if params[:refresh]
       SpectreApi.update_logins(current_user) 
@@ -31,67 +29,16 @@ class LoginsController < ApplicationController
       redirect_to logins_url
     end  
   end
-  # GET /logins/1
-  # GET /logins/1.json
-  def show
-  end
-
-  # GET /logins/new
-  def new
-    @login = Login.new
-  end
-
-  # GET /logins/1/edit
-  def edit
-  end
-
-  def login_create 
-    if current_user.customer
-      callback_url = SpectreApi.create_login(current_user.customer.customer_id, update_logins_url)
+ 
+  def create
+   if current_user.customer
+      callback_url = SpectreApi.create_login(
+                      current_user.customer.customer_id,
+                      update_logins_url)
       redirect_to callback_url
     end
   end
 
-  # POST /logins
-  # POST /logins.json
-  def create
-    #  if current_user.customer
-    #   api = Saltedge.new(CLIENT_ID, SERVICE_SECRET)
-    #   response = api.request("POST", "https://www.saltedge.com/api/v3/tokens/create", {"data" => {"customer_id" => current_user.customer.customer_id, "fetch_type" => "recent", "return_to" => "#{root_url}"}})
-    #   hash = JSON.parse(response.body)
-
-    #   redirect_to hash['data']['connect_url']
-    # end
-
-    @login = Login.new(login_params)
-
-    respond_to do |format|
-      if @login.save
-        format.html { redirect_to @login, notice: 'Login was successfully created.' }
-        format.json { render :show, status: :created, location: @login }
-      else
-        format.html { render :new }
-        format.json { render json: @login.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /logins/1
-  # PATCH/PUT /logins/1.json
-  def update
-    respond_to do |format|
-      if @login.update(login_params)
-        format.html { redirect_to @login, notice: 'Login was successfully updated.' }
-        format.json { render :show, status: :ok, location: @login }
-      else
-        format.html { render :edit }
-        format.json { render json: @login.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /logins/1
-  # DELETE /logins/1.json
   def destroy
     SpectreApi.destroy_login(@login) 
     @login.destroy
@@ -102,13 +49,7 @@ class LoginsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_login
       @login = Login.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def login_params
-      params.require(:login).permit(:login_id, :hashid, :status, :provider, :customer_id)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
   resources :transactions, only: [:index]
   resources :accounts, only: [:index]
-  resources :logins
+  resources :logins, only: [:index, :create, :destroy]
   devise_for :users
   
   root to: "logins#index"
-  get :login_create, to: 'logins#login_create'
-  get :login_refresh, to: 'logins#login_refresh'
+  get :login_refresh,   to: 'logins#login_refresh'
   get :login_reconnect, to: 'logins#login_reconnect'
-  get :update_logins, to: 'logins#update_logins'
-  get :accounts_refresh, to: 'accounts#refresh'
+  get :update_logins,   to: 'logins#update_logins'
 end
